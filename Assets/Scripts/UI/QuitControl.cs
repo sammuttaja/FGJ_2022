8d8841d42661124c40cb194b3a5cda9a8e44839b
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2022-01-29 11:56:17 +0200

    feature/quit-control: Implemented pause menu in-game.

---
+++ Assets/Scripts/UI/QuitControl.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace FGJ_2022.UI
+{
+    [AddComponentMenu("FGJ_2022/UI Quit Control")]
+    public class QuitControl : MonoBehaviour
+    {
+        [SerializeField]
+        private Button yesButton;
+
+        [SerializeField]
+        private Button noButton;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            yesButton.onClick.AddListener(ToMenu);
+            noButton.onClick.AddListener(HideQuitUI);
+        }
+
+        /// <summary>
+        /// Moves to the main menu.
+        /// </summary>
+        private void ToMenu()
+        {
+            SceneManager.LoadSceneAsync("MainMenu");
+        }
+
+        /// <summary>
+        /// Hides the quit game UI.
+        /// </summary>
+        private void HideQuitUI()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
